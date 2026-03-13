3a80f6029c80e4a47c42d39c0f1576fe2ff59b80
Ayemo Andrew <ayemoandrewgold@gmail.com>
2026-03-13 19:16:33 +0100

    added facebook pixel code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
-
-
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Lexend } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import ChatWidget from "@/components/ChatWidget";
 import "react-toastify/dist/ReactToastify.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -68,9 +67,29 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${lexend.variable} antialiased`}
       >
         {children}
-        {/* Safe Toaster */}
         <Toaster position="top-right" reverseOrder={false} containerStyle={{ zIndex: 2147483647 }}/>
         <ChatWidget />
+
+        {/* Facebook Pixel Code */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2278068076054716');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{display:'none'}}
+            src="https://www.facebook.com/tr?id=2278068076054716&ev=PageView&noscript=1"
+          />
+        </noscript>
       </body>
     </html>
   );
