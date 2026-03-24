4dbaf424df80f22948dfc929978704d350635072
Ayemo Andrew <ayemoandrewgold@gmail.com>
2026-03-24 11:48:31 +0100

    updated header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -62,13 +62,13 @@ const Header = () => {
             {profileOpen && (
               <div className="absolute right-0 mt-2 w-40 bg-white shadow-lg rounded-md p-2 space-y-2 z-50 font-[lexend]">
                 <Link
-                  href="/register"
+                  href="/"
                   className="block text-sm text-[#0E0E1D] hover:text-[#85C061] cursor-not-allowed"
                 >
                   Check Status
                 </Link>
                 <Link
-                  href="/register"
+                  href="/"
                   className="block text-sm text-[#0E0E1D] hover:text-[#85C061]"
                 >
                   Book Space
